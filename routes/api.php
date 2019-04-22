af8792c66a1dfcf1cf81f663443691e51c2f90b3
digitaltradeYazilim <yazilim@digitaltrade.com.tr>
2019-04-22 15:50:56 +0200

    api

---
+++ routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('polls','PollController@index');
+Route::get('polls/{id}','PollController@show');
+Route::post('polls','PollController@store');
+Route::put('polls/{poll}','PollController@update');
+Route::delete('polls/{poll}','PollController@delete');
+
+Route::any('errors','PollController@errors');
